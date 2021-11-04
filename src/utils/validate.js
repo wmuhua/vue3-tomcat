c1c2fa2b486eb8134015d62e6d86c0ab22b99573
wmuhua <wmuhua@126.com>
2021-11-05 00:06:28 +0800

    docs:上传图片

---
+++ src/utils/validate.js
@@ -0,0 +1,6 @@
+export function _isMobile() {
+  let flag = navigator.userAgent.match(
+    /(phone|pad|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows phone)/i
+  );
+  return flag;
+}
